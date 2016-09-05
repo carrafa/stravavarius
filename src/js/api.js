29dbc6bd5062c3879a331bb6066b291953c001d6
Joshua Carrafa <carrafa@gmail.com>
2016-09-05 14:33:42 -0400

    adds api module

---
+++ src/js/api.js
@@ -0,0 +1,34 @@
+function get(url, callback){
+  var request = new XMLHttpRequest();
+  request.open('GET', url, true);
+  request.onload = function(){
+    if( this.status >= 200 && this.status < 400 ) {
+      callback(JSON.parse(this.response));
+    }
+  };
+  request.onerror = function(){
+    console.log('error with request at ', url);
+  };
+  request.send();
+}
+
+module.exports = {
+
+  getActivities: function(callback){
+    get('/api/strava/activities', function(data){
+      callback(data);
+    });
+  },
+
+  getAthlete: function(callback){
+    get('/api/strava/athlete', function(data){
+      callback(data);
+    });
+  },
+
+  getActivityStream: function(id, callback){
+    get('/api/strava/stream/' + id, function(data){
+      callback(data);
+    });
+  }
+}
--- src/js/nav.js
+++ src/js/nav.js
@@ -1,45 +1,36 @@
 const pug = require('./pug.js');
 const Activity = require('./activity.js');
-
-function get(url, callback){
-  var request = new XMLHttpRequest();
-  request.open('GET', url, true);
-  request.onload = function(){
-    if( this.status >= 200 && this.status < 400 ) {
-      callback(JSON.parse(this.response));
-    }
-  };
-  request.onerror = function(){
-    console.log('error with request at ', url);
-  };
-  request.send();
-}
+const api = require('./api.js');
 
 let nav = {
 
   activities: function(e){
     if(e) { e.preventDefault(); }
-    get('/api/strava/activities', function(data){
+    api.getActivities(function(data){
       document.querySelector('#content > .panel.left').innerHTML = pug.activities(data);
       document.querySelectorAll('.play').forEach((link) => {
         link.addEventListener('click', nav.loadStream);
       });
     });
+
   },
 
   athlete: function(e){
-    if(e) { e.preventDefault(); }
-    get('/api/strava/athlete', function(data){
+
+    e.preventDefault();
+    api.getAthlete(function(data){
       document.querySelector('#content > .panel.left').innerHTML = pug.athlete(data);
     });
+
   },
 
   loadStream: function(e){
+
     e.preventDefault();
     let id = e.target.getAttribute('data-id');
     let stopButton = e.target.nextSibling;
-    get('/api/strava/stream/' + id, function(data){
 
+    api.getActivityStream(id, function(data){
       let activity = new Activity(data);
       activity.playSong();
 
@@ -48,6 +39,7 @@ let nav = {
         activity.stopSong();
       });
     });
+
   }
 
 };
